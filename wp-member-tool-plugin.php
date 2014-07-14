70c9ae167b99a2b3b5131f80e7d580cb55ea7a2b
Bradford Knowlton <bradford.knowlton@gmail.com>
2014-07-14 14:47:47 -0600

    github updater version header change

--- wp-member-tool-plugin.php
+++ wp-member-tool-plugin.php
@@ -5,10 +5,11 @@ Plugin Name: Daisy Member Tool Content
 Plugin URI: http://www.daisy.org/
 Description: Used by millions to make WP better.
 Author: Bradford Knowton
-Version: 1.16
+Version: 1.17.0
 Author URI: http://bradknowlton.com/
 License: GPLv2 or later
-
+GitHub Plugin URI: https://github.com/daisy/WP-member-tool-plugin
+GitHub Branch:     master
 */
 
 class WPMemberToolPlugin {
